e756704cd4f68c2480e1978e2466153bc60f39f0
Iain Ballard <iain.ballard@7digital.com>
2013-01-22 12:44:41 +0000

    stub

---
+++ src/BloatedBelly/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BloatedBelly.Properties;
+
+namespace BloatedBelly
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Testing to see if resources can be modified");
+
+			var temp = new global::System.Resources.ResourceManager("BloatedBelly.Properties.Resources", typeof(Resources).Assembly);
+
+		}
+	}
+}
