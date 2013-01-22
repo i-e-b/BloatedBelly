37d597a04bd991e2a1d44f11a5ea91774f7d557c
Iain Ballard <iain.ballard@7digital.com>
2013-01-22 13:05:40 +0000

    Proof of concept, and proof that Mono.Cecil rocks :-)

--- src/BloatedBelly/Program.cs
+++ src/BloatedBelly/Program.cs
@@ -1,19 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using BloatedBelly.Properties;
+using System.Reflection;
+using Mono.Cecil;
 
 namespace BloatedBelly
 {
-	class Program
+	public class Program
 	{
-		static void Main(string[] args)
+		const string CarriedResourceName = "Hello";
+
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("Testing to see if resources can be modified");
 
-			var temp = new global::System.Resources.ResourceManager("BloatedBelly.Properties.Resources", typeof(Resources).Assembly);
 
+			var assm = Assembly.GetExecutingAssembly();
+			foreach (var name in assm.GetManifestResourceNames())
+			{
+				if (name != CarriedResourceName) continue;
+
+				Console.WriteLine("Resource is loaded!");
+				return;
+				//var rdr = assm.GetManifestResourceStream(name);
+			}
+
+			Console.WriteLine("Resource not loaded... trying dirty hack");
+
+			var newFileName = assm.Location+".new.exe";
+			
+			var def = AssemblyDefinition.ReadAssembly(assm.Location);
+			var temp = new EmbeddedResource(CarriedResourceName, ManifestResourceAttributes.Public, 
+				new byte[] {0x01,0x02,0x03,0x04});
+			def.MainModule.Resources.Add(temp);
+			def.Write(newFileName);
+
+			Console.WriteLine("Go try the new file");
 		}
 	}
 }
